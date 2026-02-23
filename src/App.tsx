4a9b5951b4d0a515f998140b64a605f9ed22ca5f
SKEIILAT <javimacias619@gmail.com>
2026-02-22 23:06:23 -0500

    Maqueta del componente navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import { Navbar } from "./components/layout/Navbar"
 function App() {
   return (
-    <>
-        <h1 className="text-4xl text-red-500">Hola</h1>      
-    </>
+      <Navbar/>
   )
 }
 
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return(
+        <nav className="fixed top-0 right-0 left-0 bg-amber-100">
+            <a href="#home">JG.</a>
+            <ul>
+                <li><a href="#about">Sobre Mi</a></li>
+                <li><a href="#projects">Proyectos</a></li>
+                <li><a href="#skills">Habilidades</a></li>
+                <li><a href="#experience">Experiencia</a></li>
+                <li><a href="#contact">Contáctame</a></li>
+            </ul>
+        </nav>
+    )
+} 
