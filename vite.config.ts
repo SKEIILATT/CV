c704de43cc5e5821cba8ebf800eb90fcf01a91df
SKEIILAT <javimacias619@gmail.com>
2026-03-02 00:09:33 -0500

    Fixed ruta de proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/new_cv/",
+  base: "/CV/",
   plugins: [react(), tailwindcss()],
 })
